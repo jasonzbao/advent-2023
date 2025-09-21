f7a9171441be8b237c050150f4eb41d3b8954c04
Jason Bao <jason@glencoco.io>
2025-09-21 10:32:23 -0400

    remove old

--- day12/main.py
+++ day12/main.py
@@ -1,40 +1,5 @@
 from pythonutils.req import formatted_request
 
-def is_valid(spring: str, cfg: str):
-    w_i = 0
-    count = 0
-    # Add implicit separator at the end to avoid duplicate logic
-    for c in spring + ".":
-        if c == "#":
-            count += 1
-        elif c == ".":
-            if count != 0:
-                if w_i >= len(cfg) or count != cfg[w_i]:
-                    return False
-                w_i += 1
-                count = 0
-        # ? means the string hasn't finished so it's invalid
-        else:
-            return False
-    return w_i == len(cfg)
-
-def is_valid_so_far(spring: str, cfg: str):
-    w_i = 0
-    count = 0
-    for c in spring + ".":
-        if c == "#":
-            count += 1
-        elif c == ".":
-            if count != 0:
-                if w_i >= len(cfg) or count != cfg[w_i]:
-                    return False
-                w_i += 1
-                count = 0
-        # ? = we stop looking at the rest of the string, it's valid so far
-        else:
-            return True
-    return w_i == len(cfg)
-
 class Spring:
     def __init__(self, row: str, cfg: str):
         self.row = row
@@ -44,44 +9,6 @@ class Spring:
     
     def __repr__(self):
         return f"Spring(row={self.row}, working={self.working})"
-
-    # Slow brute force way, slow because we should stop early and not look at all permutations
-    # If something isn't valid
-    def possible_permutations(self):
-        question_count = self.row.count("?")
-
-        permutation_count = 0
-        for i in range(2 ** question_count):
-            binary = str(bin(i))[2:].zfill(question_count)
-            new_spring = ""
-            for c in self.row:
-                if c == "?":
-                    if binary[0] == "0":
-                        new_spring += "."
-                    else:
-                        new_spring += "#"
-                    binary = binary[1:]
-                else:
-                    new_spring += c
-            if is_valid(new_spring, self.working):
-                permutation_count += 1
-        return permutation_count
-
-    # Same as above but with pruning
-    # Ah we probably need to shrink the string so far for the cache
-    # Cache should be (string_so_far, )
-    def possible_permutations_fast(self, string_so_far: str = ""):
-        string_so_far = string_so_far or self.row
-        if is_valid(string_so_far, self.working):
-            return 1
-
-        if not is_valid_so_far(string_so_far, self.working):
-            return 0
-
-        next_question = string_so_far.index("?")
-        p1 = string_so_far[:next_question] + "." + string_so_far[next_question + 1:]
-        p2 = string_so_far[:next_question] + "#" + string_so_far[next_question + 1:]
-        return self.possible_permutations_fast(p1) + self.possible_permutations_fast(p2)
 
     def cache(func):
         def wrapper(self, *args, **kwargs):
@@ -148,7 +75,5 @@ def part2(p_input):
 
 if __name__ == "__main__":
     p_input = formatted_request(12)
-    # p_input = ["?###???????? 3,2,1"]
+    # print(part1(p_input))
     print(part2(p_input))
-
-    # print(part2(p_input))
